c4f726f7244bd29fcbd47df6129c7bc0ad2c1f92
izaratedev <izarate.developer@gmail.com>
2025-05-09 09:15:32 -0300

    Se creo una tabla en wine-list.html y se creo una variable wine en el component.ts

--- wine-cellar/src/app/app.component.ts
+++ wine-cellar/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+//Componente
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.scss'
 })
+//Esta clase, es la clase del componente
 export class AppComponent {
-  title = 'wine-cellar';
+  title = 'Wine cellar';
 }
+
---
+++ wine-cellar/src/app/wine-list/wine-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wine-list',
+  standalone: false,
+  templateUrl: './wine-list.component.html',
+  styleUrl: './wine-list.component.scss'
+})
+export class WineListComponent {
+
+  wine = {
+    name: "Alamos",
+    type: "Malbec",
+    price: 5000,
+    stock: 20
+  }
+}
