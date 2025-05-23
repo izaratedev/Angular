e434ced14b58480a5ee439860bed1d2eeeae48fc
izaratedev <izarate.developer@gmail.com>
2025-05-23 10:21:44 -0300

    Se hizo refactor de los componentes cart y wine-list agregandolos al componente winecellar-wines. Ademas se configuro el ruteo

--- wine-cellar/src/app/app-routing.module.ts
+++ wine-cellar/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WinecellarWinesComponent } from './winecellar-wines/winecellar-wines.component';
+import { WinecellarAboutComponent } from './winecellar-about/winecellar-about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //En la raiz lo que se suele hacer es definir el path vacio, y despues se puede poner un component: componente o hacer el redirecTo
+  {
+    path: '',
+    redirectTo: 'wines',
+    pathMatch: 'full'
+  },
+  //Son objetos , una vez que los agregamos se agregan los import automaticamente, si no nos lo agrega los tenemos que poner a mano
+  {
+    path: 'wines',
+    component: WinecellarWinesComponent
+  },
+  {
+    path: 'about',
+    component: WinecellarAboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
